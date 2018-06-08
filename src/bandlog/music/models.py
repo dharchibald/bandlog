b9b3e876024c8ce8996c203bade3f91c08bfea72
doug <d.archibald@gmail.com>
2018-06-08 18:02:53 -0500

    Implementing generics for labels and works

--- src/bandlog/music/models.py
+++ src/bandlog/music/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 
@@ -19,9 +21,6 @@ class Artist(models.Model):
   def __str__(self):
     return name
 
-  def get_profile(self):
-    return outline
-
 
 # Template for bands/individuals that hold more info than a name.
 class Outline(models.Model):
@@ -33,8 +32,8 @@ class Outline(models.Model):
     related_name='bands',
   )
   related = models.ManyToManyField('self', blank=True)
-  from_date = models.DateField(null=True, blank=True)
-  to_date = models.DateField(null=True, blank=True)
+  from_date = models.DateField(blank=True)
+  to_date = models.DateField(blank=True)
   discog = models.ManyToManyField('Album', blank=True)
   avg_rating = models.DecimalField(
     max_digits=3,
@@ -55,18 +54,20 @@ class Outline(models.Model):
 class Membership(models.Model):
   member = models.ForeignKey(
     Artist,
-    on_delete=models.SET_NULL,
-    null=True,
+    on_delete=models.CASCADE,
     related_name='band_memberships',
   )
   band = models.ForeignKey(
     Outline,
-    on_delete=models.SET_NULL,
-    null=True,
+    on_delete=models.CASCADE,
     related_name='member_memberships',
   )
-  from_date = models.DateField(null=True, blank=True)
-  to_date = models.DateField(null=True, blank=True)
+  from_date = models.DateField(blank=True)
+  to_date = models.DateField(blank=True)
+
+
+class Label(models.Model):
+  
 
 
 # Template for genres an album may belong to.
@@ -76,12 +77,12 @@ class Genre(models.Model):
   subgenres = models.ManyToManyField(
     'Genre',
     symmetrical=False,
-    null=True,
     blank=True,
     related_name='supergenres',
   )
 
 
+# Template for user-generated descriptors of albums/songs
 class Tag(models.Model):
   title = models.CharField(max_length=31)
   desc = models.TextField(max_length=2047, blank=True)
@@ -104,11 +105,6 @@ class Album(models.Model):
     blank=True,
     related_name='credited_on',
   )
-  genres = models.ManyToManyField(
-    Genres,
-    blank=True,
-    related_name='albums',
-  )
   release_date = models.DateField(blank=True)
   release_type = models.CharField(
     max_length=2,
@@ -126,9 +122,6 @@ class Album(models.Model):
   def __str__(self):
     return self.title
 
-  def get_type(self):
-    return self.release_type
-
 
 # Template for the songs that make up releases.
 class Song(models.Model):
@@ -141,11 +134,6 @@ class Song(models.Model):
   )
   disc_num = models.PositiveSmallIntegerField(null=True, blank=True)
   track_num = models.PositiveSmallIntegerField(null=True, blank=True)
-  avg_rating = models.DecimalField(
-    max_digits=3,
-    decimal_places=2,
-    null=True,
-  )
   sample = models.URLField(max_length=255, blank=True)
   lyrics = models.TextField(max_length=8191, blank=True)
 
@@ -153,6 +141,15 @@ class Song(models.Model):
     return self.title
 
 
+# Page dedicated to discussions between users
+class Discussion(models.Model):
+  topic = models.CharField(max_length=127)
+  orig_post = models.TextField(max_length=4095, blank=True)
+  create_time = models.DateTimeField(auto_now_add=True)
+  last_update = models.DateTimeField(auto_now=True)
+
+
+# Profile template for individual users
 class UserProfile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   friends = models.ManyToManyField(
@@ -168,49 +165,81 @@ class UserProfile(models.Model):
     blank=True,
   )
 
-# Relationship between a user and an album
-class UserAlbum(models.Model):
+
+# User listens to an album
+class AlbumListen(models.Model):
   user = models.ForeignKey(
     UserProfile,
-    on_delete=models.SET_NULL,
-    null=True,
+    on_delete=models.CASCADE,
+    related_name='albums_heard',
   )
+  listens = models.PositiveIntegerField(default=0)
+  last_listen
   album = models.ForeignKey(
     Album,
-    on_delete=models.SET_NULL,
-    null=True,
+    on_delete=models.CASCADE,
+    related_name='listeners',
   )
-  listens = PositiveIntegerField(default=0)
-  last_listen = DateField(blank=True)
-  rating = PositiveIntegerField(null=True, blank=True)
+  last_listen = models.DateField()
 
 
-# Relationship between a user and a song
+# User rating for an album/song
+class UserRating(models.Model):
   user = models.ForeignKey(
-    User,
-    on_delete=models.SET_NULL,
-    null=True,
+    UserProfile,
+    on_delete=models.CASCADE,
+    related_name='rated',
   )
-  song = models.ForeignKey(
-    Song,
-    on_delete=models.SET_NULL,
-    null=True,
+  rating = models.PositiveIntegerField()
+  last_update = models.DateField(auto_now=true)
+  content_type = models.ForeignKey(
+    ContentType,
+    on_delete=models.CASCADE,
+    related_name='ratings',
   )
-  listens = PositiveIntegerField(default=0)
-  last_listen = DateField(blank=true)
-  rating = PositiveSmallIntegerField(null=True, blank=true)
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey()
 
+
+# User vote for an album/song genre or tag
+class GenreVote(models.Model):
+  user = models.ForeignKey(
+    UserProfile,
+    on_delete=models.CASCADE,
+    related_name='genre_votes',
+  )
+  content_type = models.ForeignKey(
+    ContentType,
+    on_delete=models.CASCADE,
+    related_name='genre_votes',
+  )
+  object_id = models.PositiveIntegerField()
+  content_object = GenericForeignKey()
+  genre = models.OneToOneField(
+    Genre,
+    on_delete=models.CASCADE,
+    related_name='album_votes',
+  )
+  positive = models.BooleanField(default=true)
+
+
+class TagVote
 
 # Template for a user's post or comment
 class UserPost(models.Model):
-  user = models.ForeignKey(
+  author = models.ForeignKey(
     UserProfile,
     on_delete=models.SET_NULL,
     null=True,
     related_name='posts',
   )
-  post = models.TextField(),
+  post = models.TextField(max_length=4095),
   date = models.DateTimeField(default=timezone.now)
+  discussion = models.ForeignKey(
+    Discussion,
+    on_delete=models.CASCADE,
+    related_name='posts',
+  )
 
   def __str__(self):
     return self.post
