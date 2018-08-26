d768cba1deb8f05289863114e998438f32e7c3a4
doug <d.archibald@gmail.com>
2018-08-26 11:40:39 -0500

    blahdsjfhaj

--- src/bandlog/bandlog/settings.py
+++ src/bandlog/bandlog/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bandlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+DATABASES = {'default': {dj_database_url.config(default=DATABASE_URL)}}
 
 #DATABASES = {
 #    'default': {
