ac288e7b6e3a66a4bc31a9777947ececf26fb0da
doug <d.archibald@gmail.com>
2018-09-06 11:23:44 -0500

    See above

--- src/bandlog/music/urls.py
+++ src/bandlog/music/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('artist/new', views.artist_create, name='artist_create'),
   path('artist/<int:artist_id>/', views.artist_detail, name='artist_detail'),
   path('release/album/<int:album_id>/', views.album_detail, name='album_detail'),
   path('release/track/<int:song_id>/', views.song_detail, name='song_detail'),
