0dc7f1e7f3e4dac982d4f6797978c95969e79adf
doug <d.archibald@gmail.com>
2018-09-06 11:38:04 -0500

    Updated form with model changes

--- src/bandlog/music/forms.py
+++ src/bandlog/music/forms.py
@@ -3,13 +3,12 @@ from django import forms
 class ArtistForm(forms.ModelForm):
   class Meta:
     model = Artist
-    fields = ['first_name', 'last_name', 'from_date',
-              'to_date', 'country', 'bio',
-              'img_path']
+    fields = ['name', 'from_date', 'to_date', 
+              'country', 'bio', 'img_path']
 
 class BandForm(forms.ModelForm):
   class Meta:
     model = Artist
-    fields = ['last_name', 'members', 'from_date',
+    fields = ['name', 'members', 'from_date',
               'to_date', 'country', 'bio',
               'img_path']
